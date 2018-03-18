4808928caecda3d7887903dc0999cd5dc800d41e
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-18 16:07:18 -0600

    boilerplate done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "It works",
+  });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  res.status(404);
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(res.statusCode || 500);
+  res.json({
+    message: err.message,
+    stack: req.app.get("env") === "development" ? err.stack : {},
+  });
+});
+
+module.exports = app;
