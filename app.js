1790c1668a47201388c0dd46e98127224aac7a19
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-03-19 11:12:34 -0600

    getting api response

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const amazon = require("amazon-product-api");
 
+require("dotenv").config();
+
 const app = express();
 
 app.use(logger("dev"));
@@ -13,15 +15,55 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 
-let client = amazon.createClient({
-  awsId: "aws ID",
-  awsSecret: "aws Secret",
-  awsTag: "aws Tag",
+const client = amazon.createClient({
+  awsTag: process.env.AWS_TAG,
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET,
 });
+
+// client.itemSearch({
+//   keywords: "S.H. Figuarts",
+//   searchIndex: "Toys",
+//   responseGroup: "ItemAttributes,Offers,Images",
+// }).then(results => {
+//   console.log(results);
+// }).catch(err => {
+//   console.log(err);
+// });
+
+// app.get("/amazon/:index", function* () {
+//   this.body = yield client.itemSearch({
+//     keywords: this.query.title,
+//     searchIndex: this.params.index,
+//     responseGroup: "Images",
+//     // responseGroup: "ItemAttributes,Offers,Images",
+//   });
+// });
+
+// client.itemLookup({
+//   idType: "ASIN",
+//   itemId: "B078K3YWN3",
+// }).then(results => {
+//   console.log(JSON.stringify(results));
+// }).catch(err => {
+//   console.log(err);
+// });
+
+
+// app.get("/amazon/:index", function* () {
+//   this.body = yield client.itemLookup({
+//     idType: this.query.idType,
+//     itemId: this.params.index,
+//     responseGroup: "Images",
+//     // responseGroup: "ItemAttributes,Offers,Images",
+//   });
+//   console.log(this.body);
+// });
+
 
 app.get("/", (req, res) => {
   res.json({
-    message: "It works",
+    message: "yo",
   });
 });
 
